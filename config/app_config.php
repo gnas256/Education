1ef4fcbd33023bf5fe3990888444a52bbee48870
KIEN <hama2510@gmail.com>
2017-01-13 21:53:14 +0700

    dfd

--- config/app_config.php
+++ config/app_config.php
@@ -1,9 +1,9 @@
 <?php
 
 define("SERVER_NAME", "localhost");
-define("DATEBASE_NAME", "meet");
+define("DATEBASE_NAME", "education");
 define("USERNAME", "root");
 define("PASSWORD", "");
-define("BASE_URL", "http://localhost:8888/dating/");
+//define("BASE_URL", "http://localhost:8888/dating/");
 
 ?>
